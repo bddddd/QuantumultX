1987c24572728babc125dd2502134cd2544351a2
bddddd <50103927+bddddd@users.noreply.github.com>
2020-12-29 21:29:19 +0800

    Update IPCheck.js

--- Scripts/IPCheck.js
+++ Scripts/IPCheck.js
@@ -21,7 +21,7 @@ var flags = new Map([[ "AC" , "🇦🇨" ] , [ "AF" , "🇦🇫" ] , [ "AI" , "�
 var body = $response.body;
 var obj = JSON.parse(body);
 var title = obj['country'];
-var subtitle = '🌐'+'|'+obj['org'];
+var subtitle = '🌐 | '+obj['org'];
 var ip = obj['query'];
 var description = obj['country'] + '-' +ValidCheck(obj['city']) + '\n' + obj['org'] + '\n'+obj['ipName']+ '\n' + obj['query'];
 
